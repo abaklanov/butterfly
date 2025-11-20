a6ecc4d3bf821031672ca42dcaf419ed7f803097
abaklanov <alexandr.baklanov@gmail.com>
2025-11-20 15:16:36 +0200

    Add fastify and routes

---
+++ src/butterflies/api.ts
@@ -0,0 +1,51 @@
+import * as Fastify from 'fastify';
+import { nanoid } from 'nanoid';
+
+const butterfliesApi: Fastify.FastifyPluginCallback = function (
+  fastify,
+  _options,
+  done,
+) {
+  fastify.get('/api/butterflies', function (_request, reply) {
+    // console.log(fastify.db.data);
+    // const butterflies = fastify.db.data.butterflies || [];
+
+    reply.send({ butterflies: {} });
+  });
+
+  fastify.get<{
+    Params: {
+      id: string;
+    };
+  }>('/api/butterflies/:id', function (_request, reply) {
+    // const butterflies = fastify.db.data.butterflies || [];
+    // const butterfly = butterflies.find(
+    //   (butterfly) => butterfly.id === request.params.id,
+    // );
+    // TODO: handle not found
+    reply.send({ butterfly: {} });
+  });
+
+  fastify.post<{
+    Body: {
+      commonName: string;
+      species: string;
+      article: string;
+    };
+  }>('/api/butterflies', function (request, reply) {
+    // TODO: validate request body and return proper response
+    // TODO: validate data (e.g. non-empty strings, length limits, valid URL for article)
+    const newButterfly = {
+      id: nanoid(),
+      ...request.body,
+    };
+    // fastify.db.data.butterflies.push(newButterfly);
+
+    // fastify.db.write();
+    reply.status(201).send({ butterfly: newButterfly });
+  });
+
+  done();
+};
+
+export default butterfliesApi;
---
+++ src/users/api.ts
@@ -0,0 +1,47 @@
+import * as Fastify from 'fastify';
+import { nanoid } from 'nanoid';
+
+const usersApi: Fastify.FastifyPluginCallback = function (
+  fastify,
+  _options,
+  done,
+) {
+  // Declare a route
+  fastify.get('/api/users', function (_request, reply) {
+    // TODO: validate request
+    reply.send({ users: ['John', 'Jack', 'Jill'] });
+  });
+
+  fastify.get<{
+    Params: {
+      id: string;
+    };
+  }>('/api/users/:id', function (_request, reply) {
+    // const users = fastify.db.data.users || [];
+    // const user = users.find((user) => user.id === request.params.id);
+    // TODO: handle not found
+    reply.send({ user: {} });
+  });
+
+  fastify.post<{
+    Body: {
+      username: string;
+    };
+  }>('/api/users', function (request, reply) {
+    // TODO: validate request body and return proper response
+    // TODO: avoid duplicate usernames
+    // TODO: validate username rules (e.g. length, characters)
+    const newUser = {
+      id: nanoid(),
+      ...request.body,
+    };
+    // fastify.db.data.users.push(newUser);
+
+    // fastify.db.write();
+    reply.status(201).send({ user: newUser });
+  });
+
+  done();
+};
+
+export default usersApi;
