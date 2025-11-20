9da9049572ea3ed08f4bed37cf55a36e0b123e24
abaklanov <alexandr.baklanov@gmail.com>
2025-11-20 15:37:55 +0200

    Conteinerize

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,13 @@ const fastify = Fastify({
 fastify.register(butterfliesApi);
 fastify.register(usersApi);
 
+fastify.get('/', async function () {
+  return { hello: 'world' };
+});
+
 // TODO: use env variable for port
 // TODO: separate server from app
-fastify.listen({ port: 3000 }, function (err) {
+fastify.listen({ port: 3000, host: '0.0.0.0' }, function (err) {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
