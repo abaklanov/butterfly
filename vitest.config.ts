a1535339c5f8cef82bf48fb32495359d823e6530
abaklanov <alexandr.baklanov@gmail.com>
2025-11-22 13:54:37 +0200

    Use env for vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,18 @@
+import { loadEnv } from 'vite';
 import {
   configDefaults,
   coverageConfigDefaults,
   defineConfig,
 } from 'vitest/config';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   test: {
     exclude: [...configDefaults.exclude, 'build/**/*'],
     coverage: {
       provider: 'v8',
       exclude: [...coverageConfigDefaults.exclude, 'build/**/*'],
     },
+    // mode defines what ".env.{mode}" file to choose if exists
+    env: loadEnv(mode, process.cwd(), ''),
   },
-});
+}));
